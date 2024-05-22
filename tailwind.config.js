d9bab403bedb4a27ae57217fd4db1b2dfcea079e
Naufal Daffa Zaidan Anwar <naufalnasdra@gmail.com>
2024-05-23 06:13:09 +0700

    new repair

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "jit", //mode ini digunakan untuk custom styling seperti background yang memiliki gradient stop position, stroke, dll
-  content: ["./src/**/*.{html,js}"],
+  content: ["./dist/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
