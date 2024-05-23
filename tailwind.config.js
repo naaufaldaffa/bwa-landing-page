820d03b1eb68936034583555e039dbea33ed2cdb
Naufal Daffa Zaidan Anwar <naufalnasdra@gmail.com>
2024-05-23 12:24:18 +0700

    new era

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "jit", //mode ini digunakan untuk custom styling seperti background yang memiliki gradient stop position, stroke, dll
-  content: ["./dist/**/*.{html,js}"],
+  content: ["./dist/**/*.{html, js}"],
   theme: {
     extend: {
       fontFamily: {
